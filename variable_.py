79510d36d311b1e4bed23cdfe675d45726c9c427
seweroh <deatheater1592+4@gmail.com>
2026-06-22 07:00:05 +0000

    20260622

---
+++ variable_.py
@@ -0,0 +1,7 @@
+# variable
+# variable = value
+# save the valuein variable in the  format above.
+# = unlike the meaning of the sybol, the value is siply storted in a variable.
+# varible can be whatever you want as long as you follow the rules below.
+# 1. Imposible to start with a number
+# 2. special charecters are not possible 
