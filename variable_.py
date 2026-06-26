f6e345a85fc77760969139a2e9870419dc66c4be
seweroh <deatheater1592+4@gmail.com>
2026-06-26 07:10:04 +0000

    20260626

--- variable_.py
+++ variable_.py
@@ -4,4 +4,24 @@
 # = unlike the meaning of the sybol, the value is siply storted in a variable.
 # varible can be whatever you want as long as you follow the rules below.
 # 1. Imposible to start with a number
-# 2. special charecters are not possible 
+# 2. special charecters are not possible exept the symbol "_"
+# 3. no spacing
+# 4. reserved word not possible
+
+# my name "EVAN"
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+
+# data types (type of value)
+# str   -> String   => made up of quotes " ", ' '
+# float -> Float    => no quotes and has a decimal point
+# int   -> Integer  => no quotes and no decimal point
+# Everyday words (English, Chinese, Korean, ect. ) without quotes => error
+
+# a = 10      # saves the integer 10 in the vartiable a
+# b = 'cit'   # savees the string 'cit' in the variable b
+
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
